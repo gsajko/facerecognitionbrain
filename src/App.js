51e5099c40d08e83ed25feab0e6623b8de53967e
sajo <grzegorz.sajko@protonmail.com>
2018-09-09 21:52:32 +0200

    Added mockup of Rank component, installed Particles for a nicer background

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+    particles: {
+      number: {
+        value:70,
+        density: {
+          enable: true,
+          value_area: 800, 
+        }
+      }
+    }
+  }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}
+            />
         <Navigation />
         <Logo />
         <Rank />
         <ImageLinkForm />
         {/* 
         <FaceRecognition /> */}
-
       </div>
     );
   }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const Rank = () => {
+    return (
+        <div className='shadow-5'>
+            <div className='white f3'>
+                {'Your current rank is ...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
